fa6af2b5d5b545c14ec562c3a29748c4494c85e5
helenacuesta <hcuesta.8@gmail.com>
2019-06-06 14:17:27 +0200

    bug fixing

--- csv2pickle.py
+++ csv2pickle.py
@@ -5,9 +5,9 @@ import numpy as np
 def main(path_csv, path_pickle):
 
     x = []
-    with open(path_csv,'rb') as f:
+    with open(path_csv,'r') as f:
         reader = csv.reader(f)
         for line in reader: x.append(line)
 
-    with open(path_pickle,'wb') as f:
+    with open(path_pickle,'w') as f:
         pickle.dump(x, f, pickle.HIGHEST_PROTOCOL)
--- pickle2csv.py
+++ pickle2csv.py
@@ -6,9 +6,9 @@ import numpy as np
 def main(path_pickle,path_csv):
 
     x = []
-    with open(path_pickle,'rb') as f:
+    with open(path_pickle,'r') as f:
         x = pickle.load(f)
 
-    with open(path_csv,'wb') as f:
+    with open(path_csv,'w') as f:
         writer = csv.writer(f)
         for line in x: writer.writerow(line)
