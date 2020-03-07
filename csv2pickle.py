5480a66274a0a09294ddf1bd2077267e6ba3188c
Gazder Bence <bencegazder@gmail.com>
2020-03-07 21:53:50 +0100

    Add python3 compatibility

--- csv2pickle.py
+++ csv2pickle.py
@@ -2,12 +2,14 @@ import csv
 from six.moves import cPickle as pickle
 import numpy as np
 
-def main(path_csv, path_pickle):
+path_csv = ""
+path_pickle = ""
 
-    x = []
-    with open(path_csv,'r') as f:
-        reader = csv.reader(f)
-        for line in reader: x.append(line)
+x = []
 
-    with open(path_pickle,'w') as f:
-        pickle.dump(x, f, pickle.HIGHEST_PROTOCOL)
+with open(path_csv,'r') as f:
+    reader = csv.reader(f)
+    for line in reader: x.append(line)
+
+with open(path_pickle,'wb') as f:
+    pickle.dump(x, f, pickle.HIGHEST_PROTOCOL)
